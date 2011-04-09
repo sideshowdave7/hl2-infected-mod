f359922376ab7523035726caae7c3c4b1add87ea
mavinmartin <mavinmartin@gmail.com>
2011-04-09 16:46:17 +0000

    delete old files

--- run_hlmv.bat
+++
@@ -1,2 +0,0 @@
-@"c:\program files (x86)\steam\steamapps\sideshowdave7\sourcesdk\bin\source2007\bin\hlmv.exe" -game "c:\program files (x86)\steam\steamapps\SourceMods\cs3505mod" %1 %2 %3 %4 %5 %6 %7 %8 %9
-
--- run_mod.bat
+++
@@ -1,1 +0,0 @@
-@"c:\program files (x86)\steam\steamapps\sideshowdave7\source sdk base 2007\hl2.exe" -dev -game "c:\program files (x86)\steam\steamapps\SourceMods\cs3505mod" -allowdebug %1 %2 %3 %4 %5 %6 %7 %8 %9
