07b24956455a8d76df0bf7b02fd11cec08dcf01a
sideshowdave7 <sideshowdave7@gmail.com>
2011-04-16 17:36:18 +0000

    ok now this should really make molotov work for serious now

--- infected/src/game/server/hl2/grenade_frag.cpp
+++ infected/src/game/server/hl2/grenade_frag.cpp
@@ -59,6 +59,7 @@ public:
 	bool	IsCombineSpawned( void ) const { return m_combineSpawned; }
 	void	SetPunted( bool punt ) { m_punted = punt; }
 	bool	WasPunted( void ) const { return m_punted; }
+	void	Detonate( void );
 
 	// this function only used in episodic.
 #ifdef HL2_EPISODIC
@@ -103,6 +104,28 @@ END_DATADESC()
 //-----------------------------------------------------------------------------
 CGrenadeFrag::~CGrenadeFrag( void )
 {
+}
+
+void CGrenadeFrag::Detonate() {
+
+
+		CBaseEntity *list[1024];
+ 
+		// grab all of them within 350 units
+		int count = UTIL_EntitiesInSphere(list,1024,GetAbsOrigin(),250,MASK_PLAYERSOLID);
+ 
+		// for each of them
+		for ( int i = 0; i < count; i++ )
+		{
+			// do if statements to check what we hit ... add if player is human, etc, etc
+			if ( list[i]->IsPlayer() )
+			{
+			
+					// This will ignite the player
+					((CBasePlayer*)(list[i]))->Ignite(10.0, false, 10.0, false);
+			
+			}
+		}
 }
 
 void CGrenadeFrag::Spawn( void )
